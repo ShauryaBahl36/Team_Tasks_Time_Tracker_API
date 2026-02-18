b16eee1fb15e4085c11150d688b985d349afb5ff
Bahl_Shaurya <bahl.shaurya02@gmail.com>
2026-02-18 12:22:12 +0530

    Removed Session Authentication in settings.py and kept JWT Authentication as a way for authentication and authorization of particular users

--- drf_project/settings.py
+++ drf_project/settings.py
@@ -149,7 +149,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         # 'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
